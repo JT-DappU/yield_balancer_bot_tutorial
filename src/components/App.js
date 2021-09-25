5788f40809bf6f532870144b85bf06f6a7822517
JT-DappU <91292192+JT-DappU@users.noreply.github.com>
2021-09-24 19:38:34 -0500

    Comment Map
    Helps to build out Control, Trigger, and States into Components.

--- src/components/App.js
+++ src/components/App.js
@@ -26,23 +26,38 @@ class App extends Component {
                   <img src={logo} class="card-img px-5" alt="..."/>
                   <div class="card-body">
 
-                    {/* Web3 Found */}
+                    {/* COMPONENT
+                      * Name: Web3_Found_Screen
+                      * Imports: --
+                      * Triggers: --
+                      * Controls: -- 
+                    */}
                     <div class="web3_not_found d-none">
                       <p class="alert alert-danger">Web3 Connection Not Found</p>
                       <p class="card-text">Please download MetaMask to continue.</p>
                       <a href="#" class="btn btn-warning">Download MetaMask</a>
                     </div>
 
-                    {/* Web3 Missing */}
+                    {/* COMPONENT
+                      * Name: Connect_Web3_Screen
+                      * Imports: --
+                      * Triggers: --
+                      * Controls: connect(), 
+                    */}
                     <div class="web3_found d-none">
                       <p class="alert alert-info">Web3 Ready to Connect</p>
                       <p class="card-text">Connect to the Dapp and get started.</p>
-                      <a href="#" class="btn btn-primary">Connect to App</a>
+                      <a href="#" class="btn btn-primary">Connect to App</a> {/* connect() */}
                     </div>
 
-                    {/* Web3 Connected */}
-                    <div class="web3_connected d-none">
-                      <p class="alert alert-success">12345 DAI Available</p>
+                    {/* COMPONENT
+                      * Name: Deposit_Funds_Screen
+                      * Imports: _userAvailableDAI
+                      * Triggers: --
+                      * Controls: deposit(), 
+                    */}
+                    <div class="deposit_funds d-none">
+                      <p class="alert alert-success">12345 DAI Available</p> {/* _userAvailableDai */}
                       <div class="input-group mb-3">
                         <div class="input-group-prepend">
                           <span class="input-group-text">DAI</span>
@@ -50,46 +65,59 @@ class App extends Component {
                         <input type="number" step=".01" placeholder="TRANSFER AMOUNT" class="form-control" aria-label="DAI Stablecoin"/>
                       </div>
                       <p class="card-text">Deposit DAI into the contract.</p>
-                      <a href="#" class="btn btn-success">Deposit DAI</a>
+                      <a href="#" class="btn btn-success">Deposit DAI</a> {/* deposit() */}
                     </div>
 
-                    {/* Dapp Running */}
+                    {/* COMPONENT
+                      * Name: DappRunning_Screen
+                      * Imports: _aaveAPR, _compoundAPR, _winner, _totalEarnings
+                      * Triggers: watchTotalEarnings()
+                      * Controls: rebalance(), withdraw(),
+                    */}
                     <div class="running d-none">
-                      <p class="card-text">You are currently earning with <span class="winner">AAVE</span>.</p>
+                      <p class="card-text">You are currently earning with <span class="winner">AAVE</span>.</p> {/* _winner */}
                       <div class="media">
                         <img src="https://app.compound.finance/compound-components/assets/asset_AAVE.svg" class="col-4" alt="..."/>
                         <div class="media-body">
                           <h5 class="mt-0">AAVE</h5>
-                          <div class="alert alert-info">2.86 %APR</div>
+                          <div class="alert alert-info">2.86 %APR</div> {/* _aaveAPR */}
                         </div>
                       </div>
                       <div class="media border-top py-3">
                         <img src="https://app.compound.finance/compound-components/assets/asset_COMP.svg" class="col-4" alt="..."/>
                         <div class="media-body">
                           <h5 class="mt-0">COMPOUND</h5>
-                          <div class="alert alert-success">2.86 %APR</div>
+                          <div class="alert alert-success">2.86 %APR</div> {/* _compoundAPR */}
                         </div>
                       </div>
-                      <h5 class="alert alert-primary">Total Earnings <span class="earnings">.05</span> DAI</h5>
-                      <a href="#" class="mx-2 btn btn-info">Rebalance</a>
-                      <a href="#" class="mx-2 btn btn-danger">Withdraw</a>
+                      <h5 class="alert alert-primary">Total Earnings <span class="earnings">.05</span> DAI</h5> {/* _totalEarnings */}
+                      <a href="#" class="mx-2 btn btn-info">Rebalance</a> {/* rebalance() */}
+                      <a href="#" class="mx-2 btn btn-danger">Withdraw</a> {/* withdraw() */}
                     </div>
 
-                    {/* Rebalancing */}
+                    {/* COMPONENT
+                      * Name: Rebalancing_Screen
+                      * Imports: _rebalancingMsg, _finishRebalanceBTN
+                      * Triggers: rebalanceCheckingYields(), rebalanceTransferFunds(), rebalanceComplete(), 
+                    */}
                     <div class="rebalancing d-none">
-                      <p class="alert alert-info">Checking for the best Yield.</p>
+                      <p class="alert alert-info">Checking for the best Yield.</p> {/* _rebalancingMsg */}
                       <div class="container-fluid p-4">
                         <div class="spinner-border text-info p-5 " role="status" >
                           <span class="sr-only">Loading...</span> 
                         </div>
                       </div>
                       <p class="card-text py-2">Please wait while your rebalance is processed.</p>
-                      <a href="#" class="btn btn-primary mt-3 d-none">Continue</a>
+                      <a href="#" class="btn btn-primary mt-3 d-none">Continue</a> {/* _finishRebalanceBTN */}
                     </div>
 
-                    {/* Withdraw Funds */}
+                    {/* COMPONENT
+                      * Name: Withdraw_Screen
+                      * Imports: _contractBalance
+                      * Controls: withdrawFunds()
+                    */}
                     <div class="withdraw ">
-                      <p class="alert alert-success">12345 DAI Available</p>
+                      <p class="alert alert-success">12345 DAI Available</p> {/* _contractBalance */}
                       <div class="input-group mb-3">
                         <div class="input-group-prepend">
                           <span class="input-group-text">DAI</span>
@@ -97,7 +125,7 @@ class App extends Component {
                         <input type="number" step=".01" placeholder="WITHDRAW AMOUNT" class="form-control" aria-label="DAI Stablecoin"/>
                       </div>
                       <p class="card-text">Withdraw DAI from the contract.</p>
-                      <a href="#" class="btn btn-danger">Withdraw DAI</a>
+                      <a href="#" class="btn btn-danger">Withdraw DAI</a> {/* withdrawFunds() */}
                     </div>
 
 
