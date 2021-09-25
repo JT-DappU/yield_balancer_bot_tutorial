44464aae2a2a783258ee78d2321961ca80d04c55
JT-DappU <91292192+JT-DappU@users.noreply.github.com>
2021-09-25 18:18:08 -0500

    Create YieldBalancerBot.sol
    Note about AUTH for contract security??

---
+++ src/contracts/YieldBalancerBot.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+contract YieldBalancerBot {
+
+//CONSIDER THIS FOR AUTHENTICATION
+	// address public owner = msg.sender;
+
+	// modifier restricted() {
+	// 	require(
+	// 	msg.sender == owner,
+	// 	"This function is restricted to the contract's owner"
+	// 	);
+	// };
+
+}
